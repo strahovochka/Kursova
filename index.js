0403099af4e1876a238dc33efbdd9819f52b7eee
Codima005 <kudovin@gmail.com>
2022-06-02 02:48:39 +0300

    prikol

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const modelModule = require("./build/Release/model.node");
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
-console.log(modelModule.lemmatizer("свекруха"))
+
+const allowedOrigins = ["www.example1.com", "www.example2.com"];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      // if (allowedOrigins.indexOf(origin) === -1) {
+      //   const msg =
+      //     "The CORS policy for this site does not allow access from the specified Origin.";
+      //   return callback(new Error(msg), false);
+      // }
+      return callback(null, true);
+    },
+  })
+);
+
 app.get("/api/lemmatizer/*", (req, res) => {
   const word = req?.params[0];
   const result = modelModule.lemmatizer(word);
@@ -10,13 +26,16 @@ app.get("/api/lemmatizer/*", (req, res) => {
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify({ data: result }));
 });
+
 app.get("/api/stemmer/*", (req, res) => {
   const word = req?.params[0];
+  console.log(req?.path);
   const result = modelModule.stemmer(word);
   console.log("result", result);
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify({ data: result }));
 });
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
